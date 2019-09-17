d9cff427b0e6d6e9231d75861e4f9b9c4d935de6
hskang <whytul@gmail.com>
2019-09-18 05:07:14 +0900

    es6

---
+++ script.js
@@ -0,0 +1,6 @@
+    import { student as park, student2 as ted, student3 as abby } from './students.js';
+    const student = 'Kim';
+    console.log(student); // "Kim"
+    console.log(park);    // "Park"
+    console.log(ted);     // "Ted"
+    console.log(abby);    // "Abby"
---
+++ students.js
@@ -0,0 +1,5 @@
+export const student = 'Park';
+export const student2 = 'Ted';
+
+const student3 = 'Abby';
+export {student3};
